0f53956d092726a16e4a1c148e2b246b5c230546
jeffry1829 <jeffry1829@gmail.com>
2015-10-17 17:07:26 +0800

    upload to imgur

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 var telegram = require('telegram-bot-api');
 var request = require('request');
+var irc = require('irc');
+var imgur = require('imgur-node-api');
+
+imgur.setClientID('32cd79f0cb986fc');
+//a6c15c66c67c08
 
 var gtoken='106732837:AAHR3zt1lQLjycLSTUVu3x1Yj88ruYfumK0';
 var file='';
-var channel='YSITD';
+var channel='#ysitd';
+
+var client = new irc.Client('irc.freenode.net','teleimgbot',{
+    channels: [channel]
+});
 
 var api = new telegram({
-token: gtoken,
-updates: {
-enabled: true,
-get_interval: 1000
-}
+    token: gtoken,
+    updates: {
+		enabled: true
+	}
 });
 
 api.getMe(function(err, data)
-                {
-                console.log(err);
-                console.log(data);
-                });
+{
+    console.log(err);
+    console.log(data);
+});
 
 api.on('message', function(message)
-                {
-                console.log(message);
-                if(message.photo){
-                file = message.photo[message.photo.length-1].file_id;
-                console.log(file);
-                request('https://api.telegram.org/bot'+gtoken+'/getFile?file_id='+file,function(error, response, body){
-                        if (!error && response.statusCode == 200) {
-                        console.log(body) // Show the HTML for the Google homepage.
-                        var json=JSON.parse(body);
-                        api.sendMessage({
-chat_id: message.chat.id,
-text: 'https://api.telegram.org/file/bot'+gtoken+'/'+json.result.file_path
-});
-                        }
-                        })
+{
+    console.log(message);
+    if(message.photo){
+            file = message.photo[message.photo.length-1].file_id;
+            console.log(file);
+            request('https://api.telegram.org/bot'+gtoken+'/getFile?file_id='+file,function(error, response, body){
+                if (!error && response.statusCode == 200) {
+                    console.log(body) // Show the HTML for the Google homepage.
+                    var json=JSON.parse(body);
+                            imgur.upload('https://api.telegram.org/file/bot'+gtoken+'/'+json.result.file_path,function(err,res){
+                                if(!err){
+                                    console.log(res.data.link);
+                                    client.say(channel,res.data.link);
+                                }
+                            });
                 }
-                });
+            })
+    }
+});
